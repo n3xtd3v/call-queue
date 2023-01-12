cbbff728d422b724192dbb2a82888fbd89c27149
Pasit Panadorn <102401757+n3xtd3v@users.noreply.github.com>
2023-01-12 16:42:32 +0700

    make root route

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,57 @@
 import React, { useState, useEffect } from 'react'
-// import { useSpeechSynthesis } from 'react-speech-kit'
 
 const App = () => {
-  const [patientsData, setPatientsData] = useState(null)
-  // const { speak } = useSpeechSynthesis()
+  // const [patientsData, setPatientsData] = useState(null)
+  // const utterThis = new SpeechSynthesisUtterance()
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('src/utils/patientData.json')
-      const data = await res.json()
-      setPatientsData(data)
-    }
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch('src/utils/patientData.json')
+  //     const data = await res.json()
+  //     console.log(data)
+  //     setPatientsData(data)
+  //   }
 
-    const intervalData = setInterval(fetchData, 10000)
-    return () => clearInterval(intervalData)
-  }, [])
+  //   const intervalData = setInterval(fetchData, 10000)
+  //   return () => clearInterval(intervalData)
+  // }, [])
 
-  useEffect(() => {
-    for (let i = 0; i < patientsData?.length; i++) {
-      const patient = patientsData[i]
-      console.log(patient)
-      if (patient.current_call_queue_rcd === 'CALL') {
-        // speak({
-        //   text: patient.queue_number,
-        //   voices: {
-        //     default: true,
-        //     lang: 'th-TH',
-        //     localService: true,
-        //     name: 'Karen',
-        //     voiceURI: 'Karen',
-        //   },
-        // })
-      }
-    }
-  }, [patientsData])
+  // useEffect(() => {
+  //   const synth = window.speechSynthesis
+  //   const voices = synth.getVoices()
 
-  return <div>Hello World</div>
+  //   for (let i = 0; i < patientsData?.length; i++) {
+  //     const patient = patientsData[i]
+  //     if (patient.current_call_queue_rcd === 'CALL') {
+  //       if (patient.nationality === 'th') {
+  //         const voiceTh = voices[3]
+  //         utterThis.text = `invite number ${patient.queue_number}`
+  //         utterThis.voice = voiceTh
+  //         speechSynthesis.speak(utterThis)
+  //       } else if (patient.nationality === 'en') {
+  //         const voiceEn = voices[1]
+  //         utterThis.text = `invite number ${patient.queue_number}`
+  //         utterThis.voice = voiceEn
+  //         speechSynthesis.speak(utterThis)
+  //       }
+  //     }
+  //   }
+  // }, [patientsData])
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        fillRule="evenodd"
+        d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z"
+        clipRule="evenodd"
+      />
+    </svg>
+  )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import Root from './routes/root'
+import Floor from './routes/floor'
+import ErrorPage from './error-page'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/floor/:floorId',
+    element: <Floor />,
+    errorElement: <ErrorPage />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
