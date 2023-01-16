926505e2ec5132185276a1f2b3432a4780b0ebe8
Pasit Panadorn <102401757+n3xtd3v@users.noreply.github.com>
2023-01-16 07:59:07 +0700

    make modal

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -2,17 +2,16 @@ import { useState } from 'react'
 
 export default function Root() {
   const [isOpen, setOpen] = useState(false)
+  const [floors, setFloors] = useState('')
 
   return (
-    <div
-      className="flex justify-center items-center min-h-screen cursor-pointer bg-gray-100"
-      onClick={() => setOpen((isOpen) => !isOpen)}
-    >
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="currentColor"
-        className="animate-bounce w-96 h-96 opacity-20"
+        className="animate-bounce w-96 h-96 opacity-20 cursor-pointer"
+        onClick={() => setOpen((isOpen) => !isOpen)}
       >
         <path
           fillRule="evenodd"
@@ -20,6 +19,69 @@ export default function Root() {
           clipRule="evenodd"
         />
       </svg>
+
+      {isOpen ? (
+        <div
+          className="relative z-10"
+          aria-labelledby="modal-title"
+          role="dialog"
+          aria-modal="true"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <h3
+                      className="text-lg font-medium leading-6 text-gray-900"
+                      id="modal-title"
+                    >
+                      Floor
+                    </h3>
+                    <div className="mt-2">
+                      <form>
+                        <div className="bg-white px-4 py-5 sm:p-6">
+                          <div className="col-span-6 sm:col-span-3">
+                            <select
+                              id="floor"
+                              name="floor"
+                              className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            >
+                              <option>United States</option>
+                              <option>Canada</option>
+                              <option>Mexico</option>
+                            </select>
+                          </div>
+                        </div>
+
+                        <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                          <button
+                            type="button"
+                            className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                          >
+                            Select
+                          </button>
+                          <button
+                            type="button"
+                            className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                            onClick={() => setOpen((isOpen) => !isOpen)}
+                          >
+                            Cancel
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ''
+      )}
     </div>
   )
 }
