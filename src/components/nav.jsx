d6a23495a5bf7cf37fcca65b3199cd7f12eaeb12
Pasit Panadorn <102401757+n3xtd3v@users.noreply.github.com>
2023-02-08 12:26:29 +0700

    change layout 7

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     <div className="main-color fixed w-full top-0 left-0 z-50">
       <div className="flex justify-center items-center py-3 px-3">
-        <p className="ml-3 text-3xl text-white">
+        <p className="ml-3 text-5xl text-white">
           <span>MedPark Hospital</span>
         </p>
       </div>
